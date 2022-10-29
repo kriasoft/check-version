dcb0d99425fcfac2786cf8746992e582ec3d999c
chentingting16 <42869208+chentingting16@users.noreply.github.com>
2022-10-29 23:58:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -55,3 +55,25 @@ for (i = 0; i < s.length; i++) {
 
 console.log(actions);
 //console.log(`::set-output name=name::${actions}`);
+
+
+let connection =mysql.createConnection({
+    host: "localhost",//连接本地计算机
+    port:3306,//端口
+    user:"root",//数据库账号
+    password:"qq124519",//密码
+    database:"SchoolTownDB"//连接的数据库名
+});
+
+//调用connect方法创造连接
+connection.connect((err)=>{//回调函数,如果报错会把err填充上
+    if(err){
+        console.error("连接失败"+err.stack);//打印堆栈信息
+        return;
+    }
+    console.log("连接成功");
+});
+
+
+//关闭数据库连接
+connection.end();
