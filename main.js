607cce42bb733f0f9b6e43937542e2f5c8fd6961
chentingting16 <42869208+chentingting16@users.noreply.github.com>
2022-10-30 10:19:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -58,11 +58,11 @@ console.log(actions);
 
 
 let connection =mysql.createConnection({
-    host: "localhost",//连接本地计算机
+    host: "rm-uf60x57re73u05414go.mysql.rds.aliyuncs.com",//连接本地计算机
     port:3306,//端口
-    user:"root",//数据库账号
-    password:"qq124519",//密码
-    database:"SchoolTownDB"//连接的数据库名
+    user:"ctt",//数据库账号
+    password:"Hello123",//密码
+    database:"action"//连接的数据库名
 });
 
 //调用connect方法创造连接
