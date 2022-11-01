74bf07a14f709d52851883249d1a8f26c03f8897
chentingting16 <42869208+chentingting16@users.noreply.github.com>
2022-11-01 19:10:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ connection.query(sql2, params2, (err2,result2)=>{
     }
     if (JSON.stringify(result2) != '{}' && JSON.stringify(result2) != '[]') {
         console.log("已有配置文件"+typeof(result2));
-        
+        console.log(JSON.parse(result2));
         //配置文件与数据库中对比
         //connection.end();
         process.exit(0);
