c0e2808d17d6765238c2e6ec605b10b072b2a664
chentingting16 <42869208+chentingting16@users.noreply.github.com>
2022-11-02 21:10:34 +0800

    Update dbutil.js

--- dbutil.js
+++ dbutil.js
@@ -4,10 +4,11 @@ var mysql = require('mysql');
 
 function createConnection() {
     var connection = mysql.createConnection({
-        host: '127.0.0.1',
-        database: 'school',
-        user: 'root',
-        password: '123456'
+    host: "rm-uf60x57re73u05414go.mysql.rds.aliyuncs.com",//连接本地计算机
+    port:3306,//端口
+    user:"ctt",//数据库账号
+    password:"Hello123",//密码
+    database:"action"//连接的数据库名
     });
     return connection;
 }
